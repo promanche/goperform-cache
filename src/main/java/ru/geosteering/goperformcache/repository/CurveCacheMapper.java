1f0c0d7ddcf055c328f093e0c2a08c1bfe4b5626
Bulat <ra.bulat@gmail.com>
2022-06-16 19:44:34 +0500

    fix select query

--- src/main/java/ru/geosteering/goperformcache/repository/CurveCacheMapper.java
+++ src/main/java/ru/geosteering/goperformcache/repository/CurveCacheMapper.java
@@ -22,6 +22,6 @@ public interface CurveCacheMapper {
     @Select("select curve_id as curveId, min(first) as first, max(last) as last from curve_cache group by curve_id")
     List<MetaDataDTO> getMetaData();
 
-    @Select("select curve_id as curveId, first, last, cache from curve_cache where id = #{id} and ((first between '#{from}' and '#{to}') or (last between '#{from}' and '#{to}'))")
-    List<CurveCacheDTO> get(@Param("id") Long id, @Param("from")OffsetDateTime from, @Param("to")OffsetDateTime to);
+    @Select("select curve_id as curveId, first, last, cache from curve_cache where curve_id = #{curveId} and ((first between '#{from}' and '#{to}') or (last between '#{from}' and '#{to}'))")
+    List<CurveCacheDTO> get(@Param("curveId") Long curveId, @Param("from")OffsetDateTime from, @Param("to")OffsetDateTime to);
 }
