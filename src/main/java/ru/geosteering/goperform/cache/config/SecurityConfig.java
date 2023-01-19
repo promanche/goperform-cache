6de083b21bd86f076763d8f083fb64bfd19eff35
Bulat <ra.bulat@gmail.com>
2023-01-19 16:07:55 +0500

    Added spring-boot-actuator and by GOPRF-348

--- src/main/java/ru/geosteering/goperform/cache/config/SecurityConfig.java
+++ src/main/java/ru/geosteering/goperform/cache/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         (auth) -> auth
                                 .antMatchers("/ws").permitAll()
-                                .antMatchers("/curve/version").hasRole("USER")
+                                .antMatchers("/curve/actuator/info").hasRole("USER")
                                 .antMatchers(HttpMethod.GET, "/curve/writable").authenticated()
                                 .antMatchers("/curve/multi/**").hasRole("USER")
                                 .antMatchers(HttpMethod.POST, "/curve").hasRole("USER")
